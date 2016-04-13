61e3925fb2d5feca90d09049a0d700b2957924bd
Shirish Kadam <shirishkadam35@yahoo.com>
2016-04-13 22:10:49 +0530

    Sets Cinnamon background Daily

--- flickr.py
+++ flickr.py
@@ -1,4 +1,3 @@
-
 import datetime
 import requests
 import random
@@ -7,59 +6,86 @@ import urllib
 from os.path import expanduser
 import xml.etree.ElementTree as ET
 
-date = datetime.date.today() - datetime.timedelta(days=2)               # Flickr interseting API Works only if two days before...!?
+
+def get_interesting_image_id(furl):
+    response = requests.get(furl, stream=True)
+
+    if response.status_code != 200:
+        # This means something went wrong.
+        print("Error Code:", response.status_code)
+
+    return response.content
+
+
+def get_flickr_wallpaper_url(froot, fphotos):
+    photo_url = []
+
+    for photo in froot.findall('./photos/photo'):
+        farm_id = photo.get('farm')
+        server_id = photo.get('server')
+        photo_id = photo.get('id')
+        secret = photo.get('secret')
+        title = photo.get('title').replace(" ", "_")  # replace spaces from image name...
+        size = 'h'  # high res images
+
+        flickr_photo_url = "https://farm" + farm_id + ".staticflickr.com/" + server_id + "/" + photo_id + "_" + secret + "_" + size + ".jpg"  # static image url
+        photo_url.append(flickr_photo_url)
+        fphotos[flickr_photo_url] = title  # stores image url and image title
+
+    return random.choice(photo_url)
+
+
+def download_image_set_wallpaper(flickr_wallpaper_url, image_name):
+    urllib.urlretrieve(flickr_wallpaper_url, image_name)
+
+    linux_mint_cmd = "gsettings set org.cinnamon.desktop.background picture-uri file:///" + image_name  # Works only on Gnome 3.XX Linux Mint 17.XX Cinnamon
+    linux_mint_cmd = linux_mint_cmd.decode("utf-8")
+    print linux_mint_cmd
+    os.system(linux_mint_cmd)
+
+
+date = datetime.date.today() - datetime.timedelta(days=2)  # Flickr interseting API Works only if two days before...!?
 print(str(date))
 
-api_key = "YOUR-API-KEY"                # Insert your API key here......
+api_key = "31aa7a410f34955ca997759240d03e63"  # Insert your API key here......
 number = "5"
 
-flickr_api_test = "https://api.flickr.com/services/rest/?method=flickr.test.echo&name=value&api_key="
+# flickr_api_test = "https://api.flickr.com/services/rest/?method=flickr.test.echo&name=value&api_key="
+flickr_api = "https://api.flickr.com/services/rest/?method=flickr.interestingness.getList&date=" + str(
+        date) + "&per_page=" + number + "&api_key="
 
-flickr_api = "https://api.flickr.com/services/rest/?method=flickr.interestingness.getList&date="+str(date)+"&per_page="+number+"&api_key="
-
-url = flickr_api+api_key
+url = flickr_api + api_key
 # url = "https://api.github.com/events"
 # print(url)
 
-response = requests.get(url, stream=True)
-
-if response.status_code != 200:
-    # This means something went wrong.
-    print("Error Code:", response.status_code)
-
-xml_data = response.content             # data responded in XML
+xml_data = get_interesting_image_id(url)  # data responded in XML
 
 # xml = ET.parse("sample.xml")      if parsing from file
 # root = xml.getroot()
 
-root = ET.fromstring(xml_data)          # need to parse the response body, not the response object
-photo_url = []
+root = ET.fromstring(xml_data)  # need to parse the response body, not the response object
 photos = {'photo_url': 'photo_title'}
-
-for photo in root.findall('./photos/photo'):
-    farm_id = photo.get('farm')
-    server_id = photo.get('server')
-    photo_id = photo.get('id')
-    secret = photo.get('secret')
-    title = photo.get('title').replace(" ", "_")        # replace spaces from image name...
-    size = 'h'                                          # high res images
-
-    flickr_photo_url = "https://farm"+farm_id+".staticflickr.com/"+server_id+"/"+photo_id+"_"+secret+"_"+size+".jpg"        # static image url
-    photo_url.append(flickr_photo_url)
-    photos[flickr_photo_url] = title            # stores image url and image title
-
-flickr_wallpaper_url = random.choice(photo_url)
+flickr_wallpaper_url = get_flickr_wallpaper_url(root, photos)
 
 home = expanduser("~")
-flickr_download_dir = home+"/Pictures/Flickr"               # Flickr Image Download directory
+flickr_download_dir = home + "/Pictures/Flickr"  # Flickr Image Download directory
 
-if not os.path.exists(flickr_download_dir):                 # Make directory if not exist
+if not os.path.exists(flickr_download_dir):  # Make directory if not exist
     os.makedirs(flickr_download_dir)
 
-image_name = flickr_download_dir+"/"+photos[flickr_wallpaper_url]+"-"+str(date)+".jpg"      # Absolute path of the .jpg image
+image_name = flickr_download_dir + "/" + photos[flickr_wallpaper_url] + "-" + str(
+        date) + ".jpg"  # Absolute path of the .jpg image
 
-urllib.urlretrieve(flickr_wallpaper_url, image_name)
+today_image_set = False
 
-linux_mint_cmd = "gsettings set org.cinnamon.desktop.background picture-uri file:///"+image_name        # Works only on Gnome 3.XX Linux Mint 17.XX Cinnamon
-print linux_mint_cmd
-os.system(linux_mint_cmd)
+for img in os.listdir(flickr_download_dir):
+    if img.endswith(".jpg") and img.find(str(date)) != -1:  # download only if today's image is not present
+        today_image_set = False
+    else:
+        today_image_set = True
+
+
+if not today_image_set:
+    download_image_set_wallpaper(flickr_wallpaper_url, image_name)
+else:
+    print "Today's image already set...!"
